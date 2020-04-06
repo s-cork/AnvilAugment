73d45c6ca442bdac94fb0b7f1bc0c6b9b4c6a9e4
stucork@gmail.com <stucork@gmail.com>
2020-04-06 03:37:40 +0000

    Saved automatically

--- client_code/augment/__init__.py
+++ client_code/augment/__init__.py
@@ -1,12 +1,19 @@
 from anvil import js as _js
 
 def add_event(component, event):
+  """component: (instantiated) anvil compoent
+  event: str - any jquery event string
+  """
   if not isinstance(event, str):
     raise TypeError(f'event must be type str and not {type(event)}')
   _js.call_js('augment', component, event)
   
   
 def set_event_handler(component, event, func):
+  """component: (instantiated) anvil compoent
+  event: str - any jquery event string
+  func: function to handle the event
+  """
   add_event(component, event)
   component.set_event_handler(event, func)
   
