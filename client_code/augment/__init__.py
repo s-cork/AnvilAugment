cfc3aa029ed95663a399b37930f14d342f90a219
stucork@gmail.com <stucork@gmail.com>
2020-12-06 14:05:43 +0000

    updated to use window module

--- client_code/augment/__init__.py
+++ client_code/augment/__init__.py
@@ -16,87 +16,74 @@ import anvil as _anvil
 from anvil.js.window import jQuery as _S
 
 def add_event(component, event):
-  """component: (instantiated) anvil component
-  event: str - any jquery event string
-  """
-  init(component) # adds the trigger method to the component type
-  if not isinstance(event, str):
-      raise TypeError('event must be type str and not ' + type(event))
-  
-  _js.call_js('add_event', component, event)
-  
-  js_event_name = 'mouseenter mouseleave' if event is 'hover' else event
-  if 'key' not in event:
-      def standard_handler(e):
-          if component.raise_event(event, sender=component, event_type=e.type):
-              e.preventDefault()
+    """component: (instantiated) anvil component
+    event: str - any jquery event string
+    """
+    init(component) # adds the trigger method to the component type
+    if not isinstance(event, str):
+        raise TypeError('event must be type str and not ' + type(event))
+    _add_event(component, event)
 
-      handler = standard_handler
+    def handler(e):
+        event_args = {'sender': component, 'event_type': e.type}
+        if event.startswith('key'):
+            event_args |= {'key':e.key, 'key_code':e.keyCode, 'shift_key':e.shiftKey, 
+                           'alt_key':e.altKey, 'meta_key':e.metaKey, 'ctrl_key':e.ctrlKey}
+        if component.raise_event(event, **event_args):
+            e.preventDefault()
 
-  else:
-      def key_handler(e):
-          if component.raise_event( event, 
-                                    sender=component, 
-                                    event_type=e.type, 
-                                    key=e.key,
-                                    key_code=e.keyCode,
-                                    shift_key=e.shiftKey,
-                                    alt_key=e.altKey,
-                                    meta_key=e.metaKey,
-                                    ctrl_key=e.ctrlKey,
-                                   ):
-              e.preventDefault()
-      
-      handler = key_handler
+    js_event_name = 'mouseenter mouseleave' if event is 'hover' else event
 
-
-  _S(_get_node_for_component(component)).on(js_event_name, handler)
+    _get_jquery_for_component(component).on(js_event_name, handler)
   
   
 def set_event_handler(component, event, func):
-  """component: (instantiated) anvil compoent
-  event: str - any jquery event string
-  func: function to handle the event
-  """
-  add_event(component, event)
-  component.set_event_handler(event, func)
+    """component: (instantiated) anvil compoent
+    event: str - any jquery event string
+    func: function to handle the event
+    """
+    add_event(component, event)
+    component.set_event_handler(event, func)
 
 
 def init(component):
-  """adds a trigger method to all components of the type passed in"""
-  if isinstance(component, _Component):
-    component = type(component)
-  if issubclass(component, _Component):
-    pass
-  else:
-    raise TypeError("expected a component not {}".format(type(component).__name__))
-  if hasattr(component, 'trigger'):
-    return
-  else:
-    component.trigger = trigger
+    """adds a trigger method to all components of the type passed"""
+    if isinstance(component, _Component):
+        component = type(component)
+    elif issubclass(component, _Component):
+        pass
+    else:
+        raise TypeError("expected a component not {}".format(type(component).__name__))
+    if hasattr(component, 'trigger'):
+        return
+    else:
+        component.trigger = trigger
 
 
 def trigger(self, event):
-    """trigger an event self is an anvil component, event is a component, event is a str or a dictionary
+    """trigger an event on a component, self is an anvil component, event is a component, event is a str or a dictionary
     if event is a dictionary it should include an event key e.g. {'event': 'keypress', 'which': 13}
     """
     if isinstance(event, dict):
-        event_name = event.get('event')
-        event = _S.Event(event_name, event)
+        event = _S.Event(event['event'], event)
     event = 'mouseenter mouseleave' if event is 'hover' else event
-    _S(_get_node_for_component(self)).trigger(event)
+    _get_jquery_for_component(self).trigger(event)
 
 
-def _get_node_for_component(component):
+def _get_jquery_for_component(component):
     if isinstance(component, _anvil.Button):
-        return _js.get_dom_node(component).firstElementChild
+        return _S(_js.get_dom_node(component).firstElementChild)
     elif isinstance(component, _anvil.FileLoader):
         return _S(_js.get_dom_node(component)).find('form')
     else:
-        return _js.get_dom_node(component)
+        return _S(_js.get_dom_node(component))
   
-  
+
+def _add_event(component, event):
+    _js.call_js('add_event', component, event)
+
+
 if __name__ == '__main__':
-  _ = _anvil.Label()
-  _.set_event_handler('show', lambda **e: _anvil.Notification('oops hashrouting is a dependency', timeout=None).show())
-  _anvil.open_form(_)
+    _ = _anvil.ColumnPanel()
+    _.set_event_handler('show', lambda **e: _anvil.Notification('oops AnvilAugment is a dependency', timeout=None).show())
+    _anvil.open_form(_)
